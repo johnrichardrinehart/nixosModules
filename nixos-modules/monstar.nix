ab3a8c7b5ae1044e0375763a36cf12837f359dea
John Rinehart <johnrichardrinehart@gmail.com>
2026-09-05 16:21:28 -0400

    monstar: add configurable line scrolling

--- nixos-modules/monstar.nix
+++ nixos-modules/monstar.nix
@@ -19,6 +19,7 @@ in
     ../patches/monstar-faint-opacity.patch
     ../patches/monstar-kitty-block-shades.patch
     ../patches/monstar-ssh.patch
+    ../patches/monstar-line-scroll-bindings.patch
   ];
 
   config.nixpkgs.overlays = lib.mkAfter [
