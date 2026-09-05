48438c7a8529436a2cfa3400a9cd912902097e5d
John Rinehart <johnrichardrinehart@gmail.com>
2026-09-04 22:45:39 -0400

    chore: bump flake inputs

--- nixos-modules/monstar.nix
+++ nixos-modules/monstar.nix
@@ -19,7 +19,6 @@ in
     ../patches/monstar-faint-opacity.patch
     ../patches/monstar-kitty-block-shades.patch
     ../patches/monstar-ssh.patch
-    ../patches/monstar-osc5522-text-mime.patch
   ];
 
   config.nixpkgs.overlays = lib.mkAfter [
