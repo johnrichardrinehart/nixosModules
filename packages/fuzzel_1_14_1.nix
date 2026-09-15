f58d023b957df1f1dc5196d71530e181a665b062
John Rinehart <johnrichardrinehart@gmail.com>
2026-09-15 18:12:14 -0400

    packages: fix fuzzel 1.14.1 source hash

--- packages/fuzzel_1_14_1.nix
+++ packages/fuzzel_1_14_1.nix
@@ -4,6 +4,6 @@ fuzzel.overrideAttrs (_old: rec {
   version = "1.14.1";
   src = fetchurl {
     url = "https://codeberg.org/dnkl/fuzzel/archive/${version}.tar.gz";
-    hash = "sha256-xkFnhsOgYAuK2R7ZUcQ8ACpjmHDDgjtKYMkQRC9K4Jc=";
+    hash = "sha256-S4qRTXoGXjTafbTMauTwLHc0ReQbcksouLc4Vja0Se4=";
   };
 })
